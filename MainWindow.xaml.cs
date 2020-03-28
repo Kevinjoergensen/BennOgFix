fd65f59c57efcff26dd4b0bd747b462d6abbd16f
Kevinjoergensen <kevinjoergensen@outlook.com>
2020-03-28 20:09:47 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿
+using HomeDepotWebApp.Models;
+using HomeDepotWebApp.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeDepotDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HomeDepotContext _context;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new MainPage();
+            _context = new HomeDepotContext();
+            List<Tool> tools = _context.Tools.ToList<Tool>();
+            List<Costumer> costumers = _context.Costumers.ToList<Costumer>();
+            this.DataContext = costumers;
+        }
+
+        private void mExit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new NewC();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MainPage();
+        }
+
+        public void GoBack()
+        {
+            Main.Content = new MainPage();
+        }
+    }
+}
---
+++ NewC.xaml.cs
@@ -0,0 +1,48 @@
+﻿using HomeDepotWebApp.Models;
+using HomeDepotWebApp.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeDepotDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class NewC : Page
+    {
+        private HomeDepotContext _context;
+
+        public NewC()
+        {
+            _context = new HomeDepotContext();
+            List<Tool> tools = _context.Tools.ToList<Tool>();
+            List<Costumer> costumers = _context.Costumers.ToList<Costumer>();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Costumer nc = new Costumer();
+            nc.Name = Navn.Text;
+            nc.Password = Password.Text;
+            nc.Username = Brugernavn.Text;
+            nc.Email = Email.Text;
+            nc.Id = _context.Costumers.Count() + 1;
+            _context.Costumers.Add(nc);
+            _context.SaveChanges();
+            this.NavigationService.Content = new MainPage();
+        }
+    }
+}
